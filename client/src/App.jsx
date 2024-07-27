c07a35a226cc1e2547f06dd9b53f6d0049f44f9e
Krittika <krittikajari@gmail.com>
2024-07-28 03:19:51 +0530

    SignIn student and admin initialized; db schema created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
 import './App.css'
-// import HomePage from './components/Homepage';
+import HomePage from './components/Homepage';
 import Help from './components/Help'
+import StudentSignUp from './components/StudentSignUp'
+import AdminSignUp from './components/AdminSignUp';
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+const router = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:<HomePage/>,
+    },
+
+    {
+      path: "/help",
+      element: <Help/>,
+    },
+    //Need to get hep rou
+    // {
+    //   path:"/get-help",
+    //   element:<HomePage/>,
+    // },
+    {
+      path: "/student-signup",
+      element: <StudentSignUp/>
+    },
+    {
+      path: "/admin-signup",
+      element: <AdminSignUp/>
+    }
+  ]
+)
 
 function App() {
 
   return (
     <>
-    
-      <Help/>
+    <RouterProvider router={router} />
     </>
   )
 }
